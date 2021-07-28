0e82bbfc7be3efc7c026331040fb01f6db4733da
Drew Wilson <jankyy93@gmail.com>
2021-07-28 11:14:28 -0400

    Even or Odd

---
+++ codewars/csharp/8-kyu/Even or Odd.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Solution
+{
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else
+            {
+                return "Odd";
+            }
+        }
+    }
+}
