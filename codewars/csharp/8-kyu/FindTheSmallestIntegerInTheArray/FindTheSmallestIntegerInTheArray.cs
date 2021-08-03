da4ba26704c7f7267bc23bf2fab844787d001bce
Drew Wilson <jankyy93@gmail.com>
2021-08-03 15:51:59 -0400

    working program using LINQ

---
+++ codewars/csharp/8-kyu/FindTheSmallestIntegerInTheArray/FindTheSmallestIntegerInTheArray.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+namespace FindTheSmallestIntegerInTheArray
+{
+    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            return args.Min();
+        }
+    }
+}
---
+++ codewars/csharp/8-kyu/FindTheSmallestIntegerInTheArray/FindTheSmallestIntegerInTheArray.md
@@ -0,0 +1,4 @@
+go through each element of an array
+set a variable to a blank number space
+if an element is smaller than the variable overwrite variable with that element
+repeat until you're done with the array
